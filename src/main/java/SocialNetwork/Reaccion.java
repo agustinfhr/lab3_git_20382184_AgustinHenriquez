dbffa7f9002f772f3d0f38c115e664fedd74cadb
Agustin Henriquez <agustin.henriquez.r@usach.cl>
2021-08-12 19:23:26 -0400

    Funcionalidad visualize, comment y like creadas. Codigo Documentado y terminado

--- src/main/java/SocialNetwork/Reaccion.java
+++ src/main/java/SocialNetwork/Reaccion.java
@@ -1,12 +1,25 @@
 package SocialNetwork;
 
+/**
+ * Una clase que representa una reaccion de las publicaciones de la red social
+ * Cada reaccion queda determinada por el id, autor, fecha, tipo de reaccion y contenido
+ *
+ */
 public class Reaccion {
-    private final int id;
-    private final String autor;
-    private final String fecha;
-    private final String tipoReaccion;
-    private final String contenido;
+    private final int id; //id unico de la reaccion
+    private final String autor; //autor de la reaccion
+    private final String fecha; // fecha de la reaccion
+    private final String tipoReaccion; //tipo de reaccion (comentario o ME GUSTA)
+    private final String contenido; //contenido de la reaccion (si es ME GUSTA queda null)
 
+    /**
+     * Crea una reaccion a partir del id, autor, fecha, tipo de reaccion y contenido
+     * @param id id de la reaccion
+     * @param autor autor de la reaccion
+     * @param fecha fecha de la reaccion
+     * @param tipoReaccion tipo de reaccion (comentario o ME GUSTA)
+     * @param contenido contenido de la reaccion
+     */
     public Reaccion(int id, String autor, String fecha, String tipoReaccion, String contenido) {
         this.id = id;
         this.autor = autor;
@@ -15,16 +28,17 @@ public class Reaccion {
         this.contenido = contenido;
     }
 
-    public int getId() {
-        return this.id;
-    }
+    public String toString() {
+        //La funcionalidad puede retornar varias opciones dependiendo de la reaccion
+        //Si la reaccion corresponde a un comentario
+        if (contenido != null) {
+            return " Id: " + id + " | Tipo de reaccion: " + tipoReaccion + " | Reaccionado por: " + autor + " | En el dia: " + fecha +
+                    "\n" + " Comentario: " + contenido + "\n";
+        } else {//Si la reaccion corresponde a un ME GUSTA
+            return " Id: " + id + " | Tipo de reaccion: " + tipoReaccion + " | Reaccionado por: " + autor + " | En el dia: " + fecha + "\n";
+        }
 
-    public String getAutor() {
-        return this.autor;
-    }
 
-    public String getTipoReaccion() {
-        return this.tipoReaccion;
     }
 
 }
--- src/main/java/SocialNetwork/Socialnetwork.java
+++ src/main/java/SocialNetwork/Socialnetwork.java
@@ -1,19 +1,29 @@
-
-
 package SocialNetwork;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.Scanner;
 
-public class Socialnetwork {
-    private final ArrayList<Usuario> usuarios;
-    private final ArrayList<Publicacion> publicaciones;
-    private final ArrayList<Reaccion> reacciones;
-    private final ArrayList<Usuario> usuarioActivo;
+/**
+ * Una clase para representar la red social SocialNetwork
+ * Cada SocialNetwork queda determinado por los Usuarios, Publicaciones, Reacciones y Usuario activo
+ *
+ */
 
+public class Socialnetwork {
+
+    private final ArrayList<Usuario> usuarios; //Usuario de la red social
+    private final ArrayList<Publicacion> publicaciones; //Publicaciones de la red social
+    private ArrayList<Reaccion> reacciones; //Reacciones de la red social
+    private final ArrayList<Usuario> usuarioActivo; //Usuario activo de la red social
+
+    /**
+     * Crea un SocialNetwork a partir de los parametros ingresados: Usuarios, Publicaciones, Reacciones y Usuario Activo
+     * @param usuarios Usuarios del SocialNetwork
+     * @param publicaciones Publicaciones del SocialNetwork
+     * @param reacciones Reacciones del SocialNetwork
+     * @param usuarioActivo Usuario activo del SocialNetwork
+     */
     public Socialnetwork(ArrayList<Usuario> usuarios, ArrayList<Publicacion> publicaciones, ArrayList<Reaccion> reacciones, ArrayList<Usuario> usuarioActivo) {
         this.usuarios = usuarios;
         this.publicaciones = publicaciones;
@@ -21,73 +31,104 @@ public class Socialnetwork {
         this.usuarioActivo = usuarioActivo;
     }
 
+    /**
+     * get Usuarios
+     * @return Usuarios
+     */
+
     public ArrayList<Usuario> getUsuarios() {
         return this.usuarios;
     }
 
+    /**
+     * get Publicaciones
+     * @return Publicaciones
+     */
     public ArrayList<Publicacion> getPublicaciones() {
         return this.publicaciones;
     }
 
+    /**
+     * get Reacciones
+     * @return Reacciones
+     */
     public ArrayList<Reaccion> getReacciones() {
         return this.reacciones;
     }
 
+    /**
+     * set Reacciones
+     * @param reacciones
+     */
+    public void setReacciones(ArrayList<Reaccion> reacciones){
+        this.reacciones = reacciones;
+    }
+
+    /**
+     * get Usuario activo
+     * @return Usuario activo
+     */
     public ArrayList<Usuario> getUsuarioActivo() {
         return this.usuarioActivo;
     }
 
-    public String toString() {
-        return "\n\n---- Ususario registrados ----\n\n" + usuarios + "\n\n---- Publicaciones ----\n\n" + publicaciones;
-    }
-
+    /**
+     * Crea el registro de un nuevo Usuario a la red social
+     * @param sn SocialNetwork
+     */
     public void register(Socialnetwork sn) {
-        Scanner input1 = new Scanner(System.in);
-        Scanner input2 = new Scanner(System.in);
-        ArrayList<Usuario> listaUsuarios = sn.getUsuarios();
-        boolean registrar2 = false;
+        Scanner entrada1 = new Scanner(System.in); //Scanner para ingresar un nombre de Usuario a registrar
+        Scanner entrada2 = new Scanner(System.in); //Scanner para ingresar una contrasenia a registrar
+        ArrayList<Usuario> listaUsuarios = sn.getUsuarios(); // Se obtienen los Usuarios de la red social
+        boolean registrar = false;
 
-        while(!registrar2) {
+        while(!registrar) {
             System.out.println("\n\n Ingrese nombre de usuario a registrar: ");
-            String username = input1.next();
+            String username = entrada1.next(); // Se ingresa nombre de usuario
             System.out.println("\n Ingrese contrasenia a registrar: ");
-            String password = input2.next();
-            int id = getUsuarios().size() + 1;
-            Usuario usuarioNuevo = new Usuario(username, password, id, (ArrayList)null, (ArrayList)null,(ArrayList)null);
-            boolean encontrado = false;
+            String password = entrada2.next(); // se ingresa contrasenia
+            int id = getUsuarios().size() + 1; // se asigna un nuevo id unico al usuario
+            Usuario usuarioNuevo = new Usuario(username, password, id, (ArrayList)null, (ArrayList)null,(ArrayList)null); // Se crea al nuevo usuario
+            boolean encontrado = false; // 'encontrado' ayudara a verificar si el usuario ingresado ya habia sido registrado
 
-            for (Usuario u : listaUsuarios) {
+            for (Usuario u : listaUsuarios) { // ciclo for para recorrer los usuarios de la red social
 
-                if (u.getNombre().equals(usuarioNuevo.getNombre())) {
-                    encontrado = true;
+                if (u.getNombre().equals(usuarioNuevo.getNombre())) { //si el nombre de usuario coincide con uno de la red social (es decir, que ya se encuentra registrado)
+                    encontrado = true;                                // el verificador 'encontrado' cambiara a true
                     break;
                 }
             }
 
-            if (encontrado) {
-                System.out.println("\n *** Nombre de Usuario ya registrado *** ");
+            if (encontrado) { //Muestra error por intentar registrar un nombre de usuario ya registrado
+                System.out.println("\n ***** Nombre de Usuario YA REGISTRADO ***** ");
             } else {
-                listaUsuarios.add(usuarioNuevo);
-                System.out.println("\n\n Se ha registrado con exito! ");
-                registrar2 = true;
+                listaUsuarios.add(usuarioNuevo); //Muestra mensaje de que el nuevo usuario se ha registrado con exito
+                System.out.println("\n\n--> Se ha registrado a JAVAGRAM! <--");
+                registrar = true; //Se termina el ciclo while
             }
         }
     }
 
 
-
+    /**
+     * Crea un SocialNetwork con 5 Usuarios registrados, 10 Publicaciones, mas de 3 Follows, y 1 Follow mutuo
+     * @return Socialnetwork
+     */
     public static Socialnetwork sn1() {
+        // Se crean ArrayList para almacenar cada atributo de Socialnetwork
         ArrayList<Usuario> listaUsuarios = new ArrayList();
         ArrayList<Usuario> usuarioActivo = new ArrayList();
         ArrayList<Publicacion> listaPublicaciones = new ArrayList();
         ArrayList<Reaccion> listaReacciones = new ArrayList();
 
+        // Se crean Arraylist para almacenar los Follows de cada Usuario
         ArrayList<String> follow1 = new ArrayList();
         ArrayList<String> follow2 = new ArrayList();
         ArrayList<String> follow3 = new ArrayList();
         ArrayList<String> follow4 = new ArrayList();
         ArrayList<String> follow5 = new ArrayList();
 
+        // Se agregan los seguidores
         follow1.add("Rodrigo");
         follow2.add("Agus");
         follow2.add("Pablo");
@@ -95,6 +136,7 @@ public class Socialnetwork {
         follow4.add("Tefy");
         follow5.add("Rodrigo");
 
+        // Se crean las 10 Publicaciones
         Publicacion post1 = new Publicacion(1, (ArrayList)null, "texto", "Holaa", "10/Jul/2021", "Pablo");
         Publicacion post2 = new Publicacion(2, (ArrayList)null, "foto", "Hoy adopte un perrito", "12/Jul/2021", "Tefy");
         Publicacion post3 = new Publicacion(3, (ArrayList)null, "foto", "Aqui en la playaa B)", "12/Jul/2021", "Rodrigo");
@@ -106,33 +148,37 @@ public class Socialnetwork {
         Publicacion post9 = new Publicacion(9, (ArrayList)null, "texto", "Este año habran fondas para el 18?", "2/Ago/2021", "Gaspi");
         Publicacion post10 = new Publicacion(10, (ArrayList)null, "texto", "Ya quiero puro salir de vacaciones", "3/Ago/2021", "Agus");
 
+        // Se crean los 5 Usuarios
         Usuario user1 = new Usuario("Pablo", "4321", 1, (ArrayList)null, (ArrayList)null, follow1);
         Usuario user2 = new Usuario("Tefy", "5287", 2, (ArrayList)null, (ArrayList)null, follow2);
         Usuario user3 = new Usuario("Rodrigo", "9999", 3, (ArrayList)null, (ArrayList)null, follow3);
         Usuario user4 = new Usuario("Agus", "1234", 4, (ArrayList)null, (ArrayList)null, follow4);
         Usuario user5 = new Usuario("Gaspi", "1313", 5, (ArrayList)null, (ArrayList)null, follow5);
 
+        // Se crean las publicaiones de cada Usuario
         ArrayList<Publicacion> listaPublicaciones1 = new ArrayList();
         ArrayList<Publicacion> listaPublicaciones2 = new ArrayList();
         ArrayList<Publicacion> listaPublicaciones3 = new ArrayList();
         ArrayList<Publicacion> listaPublicaciones4 = new ArrayList();
         ArrayList<Publicacion> listaPublicaciones5 = new ArrayList();
 
+        //Se almacenan las publicaciones ya creadas en las listas de publicaciones de cada usuario
         Collections.addAll(listaPublicaciones1, new Publicacion[]{post1,post8});
         Collections.addAll(listaPublicaciones2, new Publicacion[]{post2,post5,post7});
         Collections.addAll(listaPublicaciones3, new Publicacion[]{post3,post6});
         Collections.addAll(listaPublicaciones4, new Publicacion[]{post4,post10});
         Collections.addAll(listaPublicaciones5, new Publicacion[]{post9});
-
         user1.setPublicaciones(listaPublicaciones1);
         user2.setPublicaciones(listaPublicaciones2);
         user3.setPublicaciones(listaPublicaciones3);
         user4.setPublicaciones(listaPublicaciones4);
         user5.setPublicaciones(listaPublicaciones5);
 
+        //Se almacenan los usuarios
         Collections.addAll(listaUsuarios, new Usuario[]{user1,user2,user3,user4,user5});
+        //Se almacenan la publicaciones
         Collections.addAll(listaPublicaciones, new Publicacion[]{post1,post2,post3,post4,post5,post6,post7,post8,post9,post10});
-
+        // Se retorna el Socialnetwork con todos datos ya almacenados
         return new Socialnetwork(listaUsuarios,listaPublicaciones,listaReacciones,usuarioActivo);
     }
 }
